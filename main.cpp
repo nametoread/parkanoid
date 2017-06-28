24381da3dffc60bf7313bcc1fef4baad7f46e6b2
nametoread <hacer397@gmail.com>
2017-06-29 00:21:19 +0300

    Some edits

--- main.cpp
+++ main.cpp
@@ -1,17 +1,73 @@
 #include <windows.h>
+#include <gdiplus.h>
 
-TCHAR ver[] = L"0.1-alfa";
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 700
 
-LRESULT CALLBACK wndProcess(HWND hWnd, UINT uMssg, WPARAM wParam, LPARAM lParam);
+#define BRICK_COLUMS 6
+#define BRICK_ROWS 6
+#define BRICK_WIDTH 60
+#define BRICK_HEIGHT 20
+#define BRICK_GAP 20
 
+#define BALL_SIZE 10
+
+#define PADDLE_WIDTH 10
+#define PADDLE_HEIGHT 100
+
+void exitGame(void);
+
+const TCHAR ver[] = L"0.3-alfa";
+
+LRESULT CALLBACK wndProcess(HWND hWnd, UINT uMssg, WPARAM wParam, LPARAM lParam) {
+	switch (uMssg) {
+		case WM_CREATE:			// intro alert
+/*			MessageBox(NULL, L"controls:\nmove\tleft/right\nstart\tspace\nexit\tesc",
+			L"welcome to parkanoid!", MB_ICONINFORMATION);
+*/			// initBricks();
+			break;
+		case WM_KEYDOWN:		// key press
+			switch (wParam) {
+				case VK_SPACE:			// space
+					// initBall();
+					break;
+				case VK_ESCAPE:			// esc
+					DestroyWindow(hWnd);
+					break;
+				case VK_LEFT:			// left
+					// initPaddle();
+					break;
+				case VK_RIGHT:			// right
+					// initPaddle();
+					break;
+				case VK_APPS:			// menu
+					MessageBox(NULL, L"CH34T", L"4L3RT", MB_ICONEXCLAMATION);
+					break;
+				default:				// def
+					break;
+			}
+			break;
+		case WM_CLOSE:			// destroy
+			DestroyWindow(hWnd);
+			break;
+		case WM_DESTROY:		// terminate
+/*			MessageBox(NULL, L"well played", L"exit", MB_OK);
+*/			PostQuitMessage(NULL);
+			break;
+		default:				// def
+			return DefWindowProc(hWnd, uMssg, wParam, lParam);
+	}
+
+	return NULL;
+}
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow) {
-	TCHAR ctrlClass[] = L"Control Class";
+	const TCHAR ctrlClass[] = L"Control Class";
 	WNDCLASSEX wndClass;
 	HWND hWnd;
 	MSG mMssg;
 
 	ZeroMemory(&wndClass, sizeof(wndClass));
-
+// class description - begin
 	wndClass.cbSize = sizeof(wndClass);
 	wndClass.style = NULL;
 	wndClass.lpfnWndProc = wndProcess;
@@ -24,68 +80,43 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 	wndClass.hInstance = hInst;
+// class description - end
 
 	if (!RegisterClassEx(&wndClass)) {			// class check
-		MessageBox(NULL, L"code: 10", L"error", MB_OK | MB_ICONERROR);
+		MessageBox(NULL, L"code: #1", L"error", MB_OK | MB_ICONERROR);
 		return NULL;
 	}
 
 	hWnd = CreateWindow(ctrlClass, L"parkanoid", NULL,
-		CW_USEDEFAULT, CW_USEDEFAULT, 500, 700, NULL, NULL, HINSTANCE(hInst), NULL);
+		50, 50, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, HINSTANCE(hInst), NULL);
 
 	if (!hWnd) {								// window check
-		MessageBox(NULL, L"code: 20", L"error", MB_OK | MB_ICONERROR);
+		MessageBox(NULL, L"code: #2", L"error", MB_OK | MB_ICONERROR);
 		return NULL;
 	}
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
-	while(GetMessage(&mMssg, NULL, NULL, NULL)) {
-		TranslateMessage(&mMssg);
-		DispatchMessage(&mMssg);
+	while(1) {
+		if (PeekMessage(&mMssg, NULL, NULL, NULL, PM_REMOVE)) {
+			if (mMssg.message == WM_QUIT) break;
+
+			TranslateMessage(&mMssg);
+			DispatchMessage(&mMssg);
+		}
+
+		// playGame();
 	}
 
-	return NULL;
+	exitGame();
+
+	return mMssg.wParam;
 }
 
-LRESULT CALLBACK wndProcess(HWND hWnd, UINT uMssg, WPARAM wParam, LPARAM lParam) {
-	switch (uMssg) {
-		case WM_CREATE:				// intro alert
-/*			MessageBox(NULL, L"controls:\nmove\tleft/right\nstart\tspace\nexit\tesc",
-			L"welcome to parkanoid!", MB_ICONINFORMATION);
-*/			break;
-		case WM_KEYDOWN:			// key pressed
-			switch (wParam) {
-				case VK_SPACE:			// space
-					// start
-					break;
-				case VK_ESCAPE:			// esc
-					DestroyWindow(hWnd);
-					break;
-				case VK_LEFT:			// left
-					// move
-					break;
-				case VK_RIGHT:			// right
-					// move
-					break;
-				case VK_APPS:			// menu
-					// to be continued
-					break;
-				default:				// def
-					break;
-			}
-			break;
-		case WM_CLOSE:				// destroy
-			DestroyWindow(hWnd);
-			break;
-		case WM_DESTROY:			// terminate
-/*			MessageBox(NULL, L"well played", L"exit", MB_OK);
-*/			PostQuitMessage(NULL);
-			break;
-		default:					// def
-			return DefWindowProc(hWnd, uMssg, wParam, lParam);
-	}
-	
-	return NULL;
+void exitGame(void) {
+//	MessageBox(NULL, L"exit msg", L"works", MB_OK);
+	// here
+	// destroy things after game
+	// like objects etc
 }
